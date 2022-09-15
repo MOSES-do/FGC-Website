a5af647ff8e79062241564a2adde0fcbedd38358
Moses Esumei <archer.me24@gmail.com>
2022-09-15 03:15:00 +0100

    scss updates

---
+++ js/contact.js
@@ -0,0 +1,73 @@
+const nameEl = document.querySelector("#name");
+const emailEl = document.querySelector("#email");
+const companyNameEl = document.querySelector("#company-name");
+const messageEl = document.querySelector("#message");
+const wrapper = document.querySelector(".wrapper");
+
+const form = document.querySelector("#submit-form");
+
+function checkValidations() {
+  let letters = /^[a-zA-Z\s]*$/;
+  const name = nameEl.value.trim();
+  const email = emailEl.value.trim();
+  const companyName = companyNameEl.value.trim();
+  const message = messageEl.value.trim();
+  if (name === "") {
+    document.querySelector(".name-error").classList.add("error");
+    document.querySelector(".name-error").innerText =
+      "Please fill out this field!";
+  } else {
+    if (!letters.test(name)) {
+      document.querySelector(".name-error").classList.add("error");
+      document.querySelector(".name-error").innerText =
+        "Please enter only characters!";
+    } else {
+    }
+  }
+  if (email === "") {
+    document.querySelector(".name-error").classList.add("error");
+    document.querySelector(".name-error").innerText =
+      "Please fill out this field!";
+  } else {
+    if (!letters.test(name)) {
+      document.querySelector(".name-error").classList.add("error");
+      document.querySelector(".name-error").innerText =
+        "Please enter only characters!";
+    } else {
+    }
+  }
+}
+
+function reset() {
+  nameEl = "";
+  emailEl = "";
+  companyNameEl = "";
+  messageEl = "";
+  document.querySelector(".name-error").innerText = "";
+}
+
+// On page load
+const load = document.querySelector(".contain");
+const closed = document.querySelector("#closed");
+const messenger = document.querySelector(".messenger");
+
+window.addEventListener("load", function () {
+  this.setTimeout(function open(event) {
+    load.classList.add("pop");
+  }, 17000);
+});
+
+closed.addEventListener("click", () => {
+  load.classList.remove("pop");
+});
+
+messenger.addEventListener("click", () => {
+  load.classList.toggle("pop");
+});
+
+const mediaQuery = window.matchMedia("(maxwidth: 600px)");
+
+if (mediaQuery) {
+  load.classList.add("pop");
+  closed.style.display = "none";
+}
--- js/tabbed.js
+++ js/tabbed.js
@@ -23,3 +23,35 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+// Javascript code for added div not using the same styling as the first three.
+
+// const button = document.querySelector(".btn-reveal");
+// const home = document.querySelector(".operations__content_home");
+// const tab3 = document.querySelector(".operations__content--3");
+// const tab1 = document.querySelector(".operations__tab--1");
+// const tab2 = document.querySelector(".operations__tab--2");
+// const tab3b = document.querySelector(".operations__tab--3");
+
+// const buttonReveal = () => {
+//   home.style.display = "flex";
+//   tab3.classList.toggle("operations__content--active");
+//   console.log("hello");
+//   console.log(home);
+//   console.log(button);
+//   console.log(tab3);
+// };
+
+// button.addEventListener("click", buttonReveal);
+
+// tab1.addEventListener("click", function () {
+//   home.style.display = "none";
+// });
+
+// tab2.addEventListener("click", function () {
+//   home.style.display = "none";
+// });
+
+// tab3b.addEventListener("click", function () {
+//   home.style.display = "none";
+// });
