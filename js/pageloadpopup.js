db98c018c83b9e4ef36538b077914dadec085f06
Esumei Moses <archer.me24@gmail.com>
2023-04-04 19:58:22 +0100

    updated monthly theme

--- js/pageloadpopup.js
+++ js/pageloadpopup.js
@@ -5,7 +5,7 @@ const message = document.createElement("div");
 message.classList.add("cookie-message");
 
 message.innerHTML =
-  "<marquee><b>MARCH - THEME: IN JESUS NAME </b></marquee><button class='btn--close--cookie'><span class='material-symbols-rounded'>close</span></button>";
+  "<marquee><b>APRIL - THEME: MAKING YOUR LIFE COUNT </b></marquee><button class='btn--close--cookie'><span class='material-symbols-rounded'>close</span></button>";
 
 header3.prepend(message);
 // header3.append(message); //overrides the prepend
